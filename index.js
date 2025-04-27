765003b562b3b67a6f36b66d889781bfeb2aa038
idreamfyrei <saumya.x3@gmail.com>
2025-04-27 13:40:48 +0530

    http server and log file creation

--- index.js
+++ index.js
@@ -8,3 +8,30 @@ import * as math from "./math.js";
 //for second import
 console.log(math.add(1, 2));
 console.log(math.sub(1, 2));
+
+//----------------------
+
+/*
+* Http Server
+*/ 
+
+// ! Create a log file to register every hit on server and log it. The operation is asynchronous
+
+import http from "http";
+import fs from "fs";
+
+const server = http.createServer((req, res) => {
+    const log = `${Date.now()} ${req.method} ${req.url}: New req received`;
+    fs.appendFile("log.txt", log + "\n", (err) => {
+        if (err) {
+            console.log(err);
+        }
+    res.end("Hello World");
+    })
+});
+
+server.listen(3000, () => {
+    console.log("Listening on port 3000");
+})
+
+//---------------------------------------------------------
