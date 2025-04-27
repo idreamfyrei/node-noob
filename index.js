f557021bca89c7b9c5ad10e806e4aae3fe449468
idreamfyrei <saumya.x3@gmail.com>
2025-04-27 11:48:34 +0530

    module import and export

---
+++ index.js
@@ -0,0 +1,10 @@
+// import {add,sub} from "./math.js";
+import * as math from "./math.js";
+
+//For 1st type of import
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+
+//for second import
+console.log(math.add(1, 2));
+console.log(math.sub(1, 2));
---
+++ math.js
@@ -0,0 +1,11 @@
+function add(a, b) {
+    return a + b;
+}
+function sub(a, b) {
+    return a - b;
+}
+
+//dont use const with export
+export { add, sub };
+
+
